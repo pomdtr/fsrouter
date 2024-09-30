07d88ab0eec8894667e776991da552706ed2177c
pomdtr <achille.lacoin@gmail.com>
2024-09-30 17:14:28 +0200

    spoke too soon

--- mod.ts
+++ mod.ts
@@ -141,11 +141,24 @@ function comparePatterns(a: string, b: string): number {
 
 
 
+export function normalizeRootDir(rootDir: string): string {
+    if (rootDir.startsWith("file://")) {
+        rootDir = rootDir.substring("file://".length);
+    }
+
+    if (!path.isAbsolute(rootDir)) {
+        rootDir = path.join(Deno.cwd(), rootDir);
+    }
+
+    return rootDir;
+}
 
 
 export function discoverRoutes(
     rootDir: string,
 ): http.Route[] {
+    rootDir = normalizeRootDir(rootDir);
+
     const files: string[] = [];
     try {
         for (const entry of fs.walkSync(rootDir, {
@@ -173,7 +186,8 @@ export function discoverRoutes(
                     return serveMarkdown(path.join(rootDir, filepath))
                 }
 
-                const mod = await import(`./${path.join(rootDir, filepath)}`);
+                const importUrl = new URL(`file://${path.join(rootDir, filepath)}`);
+                const mod = await import(importUrl.href);
 
                 if (!mod.default) {
                     return new Response("Handler not found", { status: 404 });
